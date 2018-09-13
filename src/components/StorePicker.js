829cba1fb638bb5e0e4ffc9f034b48f53cadb3eb
Paul Coffey <pcoffey2@gmail.com>
2018-09-13 16:45:33 +0900

    write some html with JSX

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>Hi!</p>
+        return (
+            <React.Fragment>
+                <form className="store-selector">
+                    { /* Comments */ }
+                    <h2>Please Enter A Store</h2>
+                    <input type="text" required placeholder="Store Name" />
+                    <button type="submit">Visit Store!</button>
+                </form>
+            </React.Fragment>
+        )
     }
 }
 

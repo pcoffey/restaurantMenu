f2ba3d44d8cb62213432a3418b029596ed932d25
Paul Coffey <pcoffey2@gmail.com>
2018-09-17 18:03:42 +0900

    cleanup

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyArKBuTaLc0GsZ5-f2FnF7sZ5vmTkCWD8E",
-    authDomain: "catch-of-the-day-coffey.firebaseapp.com",
-    databaseURL: "https://catch-of-the-day-coffey.firebaseio.com"
+    apiKey: "**************************",
+    authDomain: "**********************",
+    databaseURL: "**************************:"
 });
 
 const base = Rebase.createClass(firebaseApp.database());

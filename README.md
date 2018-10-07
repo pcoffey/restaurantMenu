cc93e39f7b1719a137ff43bbcdd5cc2f937e01eb
Paul Coffey <pcoffey2@gmail.com>
2018-10-07 11:56:06 +0900

    add link

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Restaurant Menu App
-
+* [Hosted on Netlify](https://adoring-minsky-efd99b.netlify.com)
 ## Check the menu for your favourite meal
 
 ## Overview
--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "*******************",
-    authDomain: ":****************",
-    databaseURL: "**********************"
+    apiKey: "AIzaSyArKBuTaLc0GsZ5-f2FnF7sZ5vmTkCWD8E",
+    authDomain: "catch-of-the-day-coffey.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-coffey.firebaseio.com"
 });
 
 const base = Rebase.createClass(firebaseApp.database());
